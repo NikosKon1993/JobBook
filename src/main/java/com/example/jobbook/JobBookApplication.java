deef944f82ff9410393683e1b5acabffd2968316
Nikos Konstas <nikkon887@gmail.com>
2021-07-19 13:14:27 +0300

    Init

---
+++ src/main/java/com/example/jobbook/JobBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobbook/JobBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
